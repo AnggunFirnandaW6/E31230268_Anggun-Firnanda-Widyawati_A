697c6d165609e1a50415cdf82a8eb9eda627ad70
AnggunFirnandaW6 <anggunfirnanda7962@gmail.com>
2024-01-30 18:52:29 +0700

    first commit

---
+++ src/sem21/Sem21.java
@@ -0,0 +1,8 @@
+package sem21;
+public class Sem21 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello anggun!!!!");
+    }
+    
+}
